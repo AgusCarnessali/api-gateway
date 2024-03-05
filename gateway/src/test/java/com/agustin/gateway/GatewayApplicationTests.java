38f0d629bdcdd7faaeb7ccfc57499d78265c1186
agustin <agustincarnessali@gmail.com>
2024-03-05 15:03:31 -0300

    api gateway with student MS

---
+++ gateway/src/test/java/com/agustin/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustin.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
